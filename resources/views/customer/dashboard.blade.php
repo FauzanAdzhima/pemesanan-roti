7b78073d1d1a1a1449efb0f46f053bb8c853721e
FauzanAdzhima <fauzanadzhima2001@gmail.com>
2022-01-03 16:18:20 +0700

    adding customer views

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -47,4 +47,220 @@
         Menu Pilihan
     </p>
 
+    <div class="card rounded mb-3 border-0 border-bottom">
+        <div class="card-body">
+            <div class="container-fluid">
+                <div class="row d-flex justify-content-center justify-items-center">
+                    @forelse ($menu as $menus)
+                    @if ($menus->status == 'Tersedia')                        
+                        @if ($menus->terjual >= 50)
+                        <div class="card mx-4 mb-3 text-center border-dark" style="width: 19rem;">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">
+                                    <img src="{{ asset($default_img='/storage/menu-images/default-food.jpg') }}" alt="{{ $menus->nama }}" class="img-thumbnail" style="width: 6em; height: 6em;">
+                                </h5>
+                                <h6 class="card-subtitle my-2 text-center">
+                                    {{ $menus->nama }}
+                                </h6>
+                                <p class="card-text text-center">
+                                    <span class="fw-bold">{{ $menus->category->nama }}</span><br/>
+                                    Harga : <span class="fw-bold">{{ $menus->harga }}</span>
+                                </p>
+                                <div class="container-fluid">                                    
+                                    <div class="row d-flex justify-items-center">
+                                        <div class="col">
+                                            <a href="{{ ($mode === 'tamu') ? '/login' : route('add-cart', $menus->id) }}"
+                                                class="btn btn-sm btn-primary">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M12.75 7.75a.75.75 0 00-1.5 0v3.5h-3.5a.75.75 0 000 1.5h3.5v3.5a.75.75 0 001.5 0v-3.5h3.5a.75.75 0 000-1.5h-3.5v-3.5z"></path><path fill-rule="evenodd" d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0z"></path></svg>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+                    @endif
+                    @empty                        
+                    @endforelse
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <p class="text-left ms-3 fw-bold ">
+        Roti
+    </p>
+
+    <div class="card rounded mb-3 border-0 border-bottom">
+        <div class="card-body">
+            <div class="container-fluid">
+                <div class="row d-flex justify-content-center justify-items-center">
+                    @forelse ($menu as $menus)
+                    @if ($menus->status == 'Tersedia')                        
+                        @if ($menus->category_id == '1')
+                        <div class="card mx-4 mb-3 text-center border-dark" style="width: 19rem;">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">
+                                    <img src="{{ asset($default_img='/storage/menu-images/default-food.jpg') }}" alt="{{ $menus->nama }}" class="img-thumbnail" style="width: 6em; height: 6em;">
+                                </h5>
+                                <h6 class="card-subtitle my-2 text-center">
+                                    {{ $menus->nama }}
+                                </h6>
+                                <p class="card-text text-center">
+                                    <span class="fw-bold">{{ $menus->category->nama }}</span><br/>
+                                    Harga : <span class="fw-bold">{{ $menus->harga }}</span>
+                                </p>
+                                <div class="container-fluid">                                    
+                                    <div class="row d-flex justify-items-center">
+                                        <div class="col">
+                                            <a href="{{ ($mode === 'tamu') ? '/login' : route('add-cart', $menus->id) }}"
+                                                class="btn btn-sm btn-primary">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M12.75 7.75a.75.75 0 00-1.5 0v3.5h-3.5a.75.75 0 000 1.5h3.5v3.5a.75.75 0 001.5 0v-3.5h3.5a.75.75 0 000-1.5h-3.5v-3.5z"></path><path fill-rule="evenodd" d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0z"></path></svg>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+                    @endif
+                    @empty                        
+                    @endforelse
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <p class="text-left ms-3 fw-bold ">
+        Cake
+    </p>
+
+    <div class="card rounded mb-3 border-0 border-bottom">
+        <div class="card-body">
+            <div class="container-fluid">
+                <div class="row d-flex justify-content-center justify-items-center">
+                    @forelse ($menu as $menus)
+                    @if ($menus->status == 'Tersedia')
+                        @if ($menus->category_id == '2')
+                        <div class="card mx-4 mb-3 text-center border-dark" style="width: 19rem;">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">
+                                    <img src="{{ asset($default_img='/storage/menu-images/default-food.jpg') }}" alt="{{ $menus->nama }}" class="img-thumbnail" style="width: 6em; height: 6em;">
+                                </h5>
+                                <h6 class="card-subtitle my-2 text-center">
+                                    {{ $menus->nama }}
+                                </h6>
+                                <p class="card-text text-center">
+                                    <span class="fw-bold">{{ $menus->category->nama }}</span><br/>
+                                    Harga : <span class="fw-bold">{{ $menus->harga }}</span>
+                                </p>
+                                <div class="container-fluid">                                    
+                                    <div class="row d-flex justify-items-center">
+                                        <div class="col">
+                                            <a href="{{ ($mode === 'tamu') ? '/login' : route('add-cart', $menus->id) }}"
+                                                class="btn btn-sm btn-primary">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M12.75 7.75a.75.75 0 00-1.5 0v3.5h-3.5a.75.75 0 000 1.5h3.5v3.5a.75.75 0 001.5 0v-3.5h3.5a.75.75 0 000-1.5h-3.5v-3.5z"></path><path fill-rule="evenodd" d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0z"></path></svg>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endif                        
+                    @endif
+                    @empty                        
+                    @endforelse
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <p class="text-left ms-3 fw-bold ">
+        Makanan
+    </p>
+
+    <div class="card rounded mb-3 border-0 border-bottom">
+        <div class="card-body">
+            <div class="container-fluid">
+                <div class="row d-flex justify-content-center justify-items-center">
+                    @forelse ($menu as $menus)
+                    @if ($menus->status == 'Tersedia')                        
+                        @if ($menus->category_id == '3')
+                        <div class="card mx-4 mb-3 text-center border-dark" style="width: 19rem;">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">
+                                    <img src="{{ asset($default_img='/storage/menu-images/default-food.jpg') }}" alt="{{ $menus->nama }}" class="img-thumbnail" style="width: 6em; height: 6em;">
+                                </h5>
+                                <h6 class="card-subtitle my-2 text-center">
+                                    {{ $menus->nama }}
+                                </h6>
+                                <p class="card-text text-center">
+                                    <span class="fw-bold">{{ $menus->category->nama }}</span><br/>
+                                    Harga : <span class="fw-bold">{{ $menus->harga }}</span>
+                                </p>
+                                <div class="container-fluid">                                    
+                                    <div class="row d-flex justify-items-center">
+                                        <div class="col">
+                                            <a href="{{ ($mode === 'tamu') ? '/login' : route('add-cart', $menus->id) }}"
+                                                class="btn btn-sm btn-primary">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M12.75 7.75a.75.75 0 00-1.5 0v3.5h-3.5a.75.75 0 000 1.5h3.5v3.5a.75.75 0 001.5 0v-3.5h3.5a.75.75 0 000-1.5h-3.5v-3.5z"></path><path fill-rule="evenodd" d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0z"></path></svg>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+                    @endif
+                    @empty                        
+                    @endforelse
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <p class="text-left ms-3 fw-bold ">
+        Minuman
+    </p>
+
+    <div class="card rounded mb-3 border-0 border-bottom">
+        <div class="card-body">
+            <div class="container-fluid">
+                <div class="row d-flex justify-content-center justify-items-center">
+                    @forelse ($menu as $menus)
+                    @if ($menus->status == 'Tersedia')                        
+                        @if ($menus->category_id == '4')
+                        <div class="card mx-4 mb-3 text-center border-dark" style="width: 19rem;">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">
+                                    <img src="{{ asset($default_img='/storage/menu-images/default-food.jpg') }}" alt="{{ $menus->nama }}" class="img-thumbnail" style="width: 6em; height: 6em;">
+                                </h5>
+                                <h6 class="card-subtitle my-2 text-center">
+                                    {{ $menus->nama }}
+                                </h6>
+                                <p class="card-text text-center">
+                                    <span class="fw-bold">{{ $menus->category->nama }}</span><br/>
+                                    Harga : <span class="fw-bold">{{ $menus->harga }}</span>
+                                </p>
+                                <div class="container-fluid">                                    
+                                    <div class="row d-flex justify-items-center">
+                                        <div class="col">
+                                            <a href="{{ ($mode === 'tamu') ? '/login' : route('add-cart', $menus->id) }}"
+                                                class="btn btn-sm btn-primary">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M12.75 7.75a.75.75 0 00-1.5 0v3.5h-3.5a.75.75 0 000 1.5h3.5v3.5a.75.75 0 001.5 0v-3.5h3.5a.75.75 0 000-1.5h-3.5v-3.5z"></path><path fill-rule="evenodd" d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0z"></path></svg>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+                    @endif
+                    @empty                        
+                    @endforelse
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
--- resources/views/customer/navbar.blade.php
+++ resources/views/customer/navbar.blade.php
@@ -1,17 +1,25 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-warning bg-gradient">
     <div class="container">
-        <a class="navbar-brand" href="/">De Island Bakery</a>
+        <a class="navbar-brand" href="{{ ($mode === 'tamu') ? '/' : '/customer' }}">De Island Bakery</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav px-4 text-decoration-underline">
-                <div class="col-md-6" style="margin:0 10vw 0 0;"></div>
-                <a class="nav-link fw-bold {{ ($page === 'Home') ? 'active' : '' }}" href="/">Home</a>
-                <div class="" style="margin:0 0 0 0;"></div>
+            <div class="navbar-nav px-4">
+                <div class="" style="min-width: 20em;"></div>
+                <a class="nav-link fw-bold {{ ($page === 'Home') ? 'active' : '' }}" href="{{ ($mode === 'tamu') ? '/' : '/customer' }}">Home</a>
+                <div class="" style="min-width: 1rem;"></div>
                 <a class="nav-link fw-bold {{ ($page === 'Kontak Kami') ? 'active' : '' }}" href="/about">Kontak</a>
-                <div class="col-md-6" style="margin:0 0 0 10vw;"></div>                                  
-                <a class="nav-link fw-bold {{ ($page === 'Login') ? 'active' : '' }}" href="/login">Login</a>
-            </div>        
+                @if ( $mode === 'tamu' )
+                    <div class="" style="min-width: 25em;"></div>
+                    <a class="nav-link fw-bold {{ ($page === 'Login') ? 'active' : '' }}" href="/login">Login</a>
+                @else                
+                    <div class="" style="min-width: 20em;"></div>
+                    <a class="nav-link fw-bold" href="{{ route('customer-logout') }}">Cart</a>
+                    <div class="" style="min-width: 1rem;"></div>
+                    <a class="nav-link fw-bold {{ ($page === 'Profil Pelanggan') ? 'active' : '' }}" href="{{ route('customer-profile') }}">Profil</a>
+                    <div class="" style="min-width: 1rem;"></div>
+                    <a class="nav-link fw-bold" href="{{ route('customer-logout') }}">Logout</a>
+                @endif
     </div>
 </nav>
